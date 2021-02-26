178d3cf4c004985ea732d38011bf87b93070f134
Philip Pages <philip.pages@gmail.com>
2021-02-25 18:50:10 -0600

    added event listeners for all the buttons

---
+++ main.js
@@ -0,0 +1,91 @@
+
+
+//button event listeners
+
+document.getElementById(`clear-all`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`clear-current`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`percent`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`plus-minus`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`exponent`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`divide`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`1`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`2`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`3`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`4`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`5`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`6`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`7`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`8`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`9`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`remove`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`multiply`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`subtract`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`add`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`zero`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`dot`). addEventListener(`click`, function() {
+
+});
+
+document.getElementById(`equal`). addEventListener(`click`, function() {
+
+});
